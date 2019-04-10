df3828ea6ba14932c94a30cadbdbfed10286c7c0
Paul Devine <business@pdevine.com>
2019-04-10 04:10:00 +0000

    making mp4, sorting input files

--- stich_images.py
+++ stich_images.py
@@ -2,21 +2,21 @@ import cv2
 import numpy as np
 import os
 
-TEST_DIR='/media/bernal-tensor/out_VTS_03_01_images'
+TEST_DIR='/media/bernal-tensor/Video_4_Paul/520_1140_process'
 img=[]
 os.chdir(TEST_DIR) 
 jpgs = [x for x in os.listdir(TEST_DIR) if x.endswith('.jpg')]
+jpgs.sort()
 print('jpgs[1]:i{}'.format(jpgs[0]))
-for jpg in jpgs:
-    img.append(cv2.imread(jpg))
+sample = cv2.imread(jpgs[1])
 
-height,width,layers=img[1].shape
+height,width,layers=sample.shape
 
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-writer=cv2.VideoWriter('test.mp4',fourcc,30.0,(720,480))
+writer=cv2.VideoWriter('517_1150.mp4',fourcc,30.0,(width,height))
 
-for image in img:
-    writer.write(image)
+for jpg in jpgs:
+    writer.write(cv2.imread(jpg))
 
 cv2.destroyAllWindows()
 writer.release()
