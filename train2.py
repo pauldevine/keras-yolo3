4bae5fd2488ede9b16dabb80dc764226ee27c546
Paul Devine <business@pdevine.com>
2019-04-26 05:26:40 +0000

    fixing train2.py for multi gpu

--- train2.py
+++ train2.py
@@ -65,9 +65,9 @@ def _main():
         for i in range(len(model.layers)):
             model.layers[i].trainable = True
         if USE_SGDAccum:
-            model.compile(optimizer=SGDAccum(lr=MIN_LR, accum_iters=ACCUM_ITERS), loss={'yolo_loss': lambda y_true, y_pred: y_pred}) # recompile to apply the change
+            model.compile(optimizer=SGDAccum(lr=MIN_LR, accum_iters=ACCUM_ITERS), loss={'yolo_loss': lambda y_true, y_pred: y_pred[0]}) # recompile to apply the change
         else:
-            model.compile(optimizer=SGD(lr=MIN_LR), loss={'yolo_loss': lambda y_true, y_pred: y_pred}) # recompile to apply the change
+            model.compile(optimizer=SGD(lr=MIN_LR), loss={'yolo_loss': lambda y_true, y_pred: y_pred[0]}) # recompile to apply the change
 
         print('Train on {} samples, val on {} samples, with batch size {}.'.format(num_train, num_val, batch_size))
         model.fit_generator(data_generator_wrapper(lines[:num_train], batch_size, input_shape, anchors, num_classes),
